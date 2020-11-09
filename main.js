3f65f47a3fc2ae1077cd36318ecb09e02a8fd4b4
fredrikpihlgren <70364712+fredrikpihlgren@users.noreply.github.com>
2020-11-09 18:23:05 +0100

    dataExtractor
    Added function that can easily extract data from the API

--- main.js
+++ main.js
@@ -135,7 +135,10 @@ let showProduct2 = (beerId) => {
     let imgElement = document.createElement("img");
     imgElement.className = "showimg";
     imgElement.alt = "bild:";
-    imgElement.src = beerId[0].image_url;
+    imgElement.src=noPic;
+    if (beerId[0].image_url != null) {
+        imgElement.src = beerId[0].image_url;
+    }
     card.appendChild(imgElement);
 
     
@@ -168,20 +171,53 @@ let showProduct2 = (beerId) => {
     eContainer2.appendChild(infoElement);
     infoElement.className = "showinfo";
 */
+
+    let malt_add = [];
+    let hops_add = [];
+    let foods_add = [];
+
+    console.log(beerId[0].food_pairing);
+
+    extractData(beerId[0].ingredients.malt, malt_add, true);
+    extractData(beerId[0].ingredients.hops, hops_add, true);
+    extractData(beerId[0].food_pairing, foods_add, false);
+
+
+
+    /*
+    malt_get.forEach(element => {
+        console.log(element);
+        let myString=" "+element.name+" ("+element.amount.value+" "+element.amount.unit+")";
+        malt_add.push(myString);
+    });
+    */
+    
+
+    //const hops = beerId[0].ingredients.hops;
+    //console.log(malt);
+
+    //array1.forEach(element => console.log(element));
+
+
     let infoData = [
         { desc: "Alcohol by volume", val: beerId[0].abv },
         { desc: "Volume", val: beerId[0].volume.value + " " + beerId[0].volume.unit },
-        { desc: "Ingredients", val: "tttt" },
-        { desc: "Hops", val: "N/A" },
-        { desc: "Food pairing", val: "N/A" },
-        { desc: "Brewers tips", val: "N/A" }
+        { desc: "Ingredients", val: "MALTS: "+malt_add+" YEAST: "+beerId[0].ingredients.yeast },
+        { desc: "Hops", val: hops_add },
+        { desc: "Food pairing", val: foods_add },
+        { desc: "Brewers tips", val: beerId[0].brewers_tips }
     ];
 
     for (let i = 0; i < infoData.length; i++) {
         let optElement = document.createElement("li");
         optElement.className = "showoption";
-        optElement.textContent = infoData[i].desc + ": " + infoData[i].val;
+        let hElement = document.createElement("span");
+        hElement.className = "nah";
+        optElement.textContent = infoData[i].desc + ": ";
+        hElement.textContent = infoData[i].val;
+        hElement.style.color="red";
         infoElement.appendChild(optElement);
+        optElement.appendChild(hElement);
     }
 
     cardContainer.appendChild(infoElement);
@@ -192,7 +228,17 @@ let showProduct2 = (beerId) => {
 }
 
 
-
+let extractData = (to_get, to_add, classt) => {
+    let myString;
+    to_get.forEach(element => {
+        console.log(element);
+        if (classt == true) {
+            myString=" "+element.name+" ("+element.amount.value+" "+element.amount.unit+")"; 
+        }
+        else {myString=" "+element;}
+        to_add.push(myString);
+    });
+}
 
 
 
